e796d6b86df5c163ac8a5d187e28682898df3dd4
dulltz <isrgnoe@gmail.com>
2015-08-26 18:09:07 +0900

    tweetし続けるバグを修正

--- chrome_extension/javascripts/background.js
+++ chrome_extension/javascripts/background.js
@@ -10,21 +10,22 @@ var timerID = setTimeout(function mainLoop() {
     clearTimeout(timerID);
 
     var url = currentTab.url;
-    console.log(url);
 
-    if (isNgSite(previousUrl) && isNgSite(url)) cumulativeVisitDuration++;
-    previousUrl = url;
-
+    if (isNgSite(previousUrl) && isNgSite(url)) {
+      cumulativeVisitDuration++;
+    }
     if (cumulativeVisitDuration == 5) {
-        alert("あと5秒ニコニコ動画に滞在するとTwitterに報告されます");
+      alert("あと5秒ニコニコ動画に滞在するとTwitterに報告されます");
     }
     if (cumulativeVisitDuration == 10) {
-        tweet("有言不実行！ " + new Date().toString());
+      tweet("有言不実行！ " + new Date().toString());
+      cumulativeVisitDuration++;
     }
     if (cumulativeVisitDuration >= 10 && isNgSite(url)) {
-        chrome.tabs.update(currentTab.id, {url: "chrome://newtab"});
+      chrome.tabs.update(currentTab.id, {url: "chrome://newtab"});
+      cumulativeVisitDuration = 0;
     }
-
+    previousUrl = url;
     timerID = setTimeout(mainLoop, 1000);
 }, 0);
 
