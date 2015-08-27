ce9cd969297ad66a1feca888a21925dd5692d6ef
strangestorage <watanabe@watanaberei-no-MacBook-Air.local>
2015-08-27 15:02:56 +0900

    タイマーの時間指定機能＆タスク完了ボタンを追加

--- chrome_extension/javascripts/popup.js
+++ chrome_extension/javascripts/popup.js
@@ -1,17 +1,43 @@
 var form = document.createElement("form");
-var btn = document.createElement("input");
-btn.id = "button1";
-btn.type = "button";
-btn.value = "監視スタート";
-form.appendChild(btn);
+
+var timeText = document.createElement("input");
+timeText.id = "timetext";
+timeText.type = "text";
+timeText.value = "10";
+form.appendChild(timeText);
 document.body.appendChild(form);
-btn.addEventListener("click", callBackGround, false);
+
+var startButton = document.createElement("input");
+startButton.id = "startbutton";
+startButton.type = "button";
+startButton.value = "監視スタート";
+form.appendChild(startButton);
+document.body.appendChild(form);
+startButton.addEventListener("click", callBackGround, false);
+
+var endButton = document.createElement("input");
+endButton.id = "endbutton";
+endButton.type = "button";
+endButton.value = "タスク完了";
+form.appendChild(endButton);
+document.body.appendChild(form);
+endButton.addEventListener("click", stopTimer, false);
 
 window.onload = function(){
-	//alert("popup.htmlのonloadです！");
+    //alert("popup.htmlのonloadです！");
 };
 
 function callBackGround(){
-	var bg = chrome.extension.getBackgroundPage();
-	bg.mainLoop();
+    var bg = chrome.extension.getBackgroundPage();
+    var time = Number(timeText.value) * 60;
+    console.log(time);
+    timeText.style.visibility = "hidden";
+    startButton.style.visibility = "hidden";
+    //bg.setTimer(time);
+    //bg.mainLoop();
+}
+
+function stopTimer(){
+    var bg = chrome.extension.getBackgroundPage();
+    //bg.stopTimer();
 }
