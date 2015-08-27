62b6ada0924e51122566b748d92aff88681e0a96
strangestorage <watanabe@watanaberei-no-MacBook-Air.local>
2015-08-27 17:13:31 +0900

    書き換えられるだけJQueryで書き換えしました

--- chrome_extension/javascripts/popup.js
+++ chrome_extension/javascripts/popup.js
@@ -5,24 +5,17 @@ var form = document.createElement("form");
 var taskTimeText = document.createElement("input");
 $(taskTimeText).val("10").attr("type","text").attr("id","tasktimetext");
 var minutesText = document.createTextNode("分");
-var br = document.createElement("br");
-form.appendChild(taskTimeText);
-form.appendChild(minutesText);
-form.appendChild(br);
-document.body.appendChild(form);
+$(form).append(taskTimeText).append(minutesText).append($("<br>"));
 
 var startButton = document.createElement("input");
-$(startButton).val("監視スタート").attr("type","button").attr("id","startbutton");
-if(!bg.startButtonVisible)startButton.style.visibility = "hidden";
-form.appendChild(startButton);
-document.body.appendChild(form);
-startButton.addEventListener("click", callBackGround, false);
+$(startButton).val("監視スタート").attr("type","button").attr("id","startbutton").bind("click",callBackGround);
+$(form).append(startButton);
 
 var endButton = document.createElement("input");
-$(endButton).val("タスク完了").attr("type","button").attr("id","endbutton");
-form.appendChild(endButton);
+$(endButton).val("タスク完了").attr("type","button").attr("id","endbutton").bind("click",stopTimer);
+$(form).append(endButton);
+
 document.body.appendChild(form);
-endButton.addEventListener("click", stopTimer, false);
 
 window.onload = function(){
     //alert("popup.htmlのonloadです！");
