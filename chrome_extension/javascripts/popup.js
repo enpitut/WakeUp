af8a857e27f516432fb8e7ccf6676caebbb29eca
Hiroki FUKUDA <zzzfuku1991@gmail.com>
2015-09-11 00:04:15 +0900

    監視スタートボタンを押したら時間指定テキストエリアやスタートボタンを非表示にする機能を実装

--- chrome_extension/javascripts/popup.js
+++ chrome_extension/javascripts/popup.js
@@ -1,18 +1,26 @@
-var bg = chrome.extension.getBackgroundPage();
-
 $(function () {
+    var bg = chrome.extension.getBackgroundPage();
+    function flushButtonArea() {
+        if (bg.isTimerOn) {
+            $("#start_button").parent().css("display", "none");
+            $("#end_button").parent().css("display", "block");
+        } else {
+            $("#start_button").parent().css("display", "block");
+            $("#end_button").parent().css("display", "none");
+        }
+    }
+
     $("#start_button").click(function () {
         var time = Number($("#task_time_text").val()) * 60;
-        if(time < 0) return false;
-        chrome.browserAction.setIcon({path: "../images/watchicon16.png"});
+        if(isNaN(time) || time < 0) return false;
         bg.startTimer(time);
+        flushButtonArea();
     });
     $("#end_button").click(function () {
-        chrome.browserAction.setIcon({path: "../images/icon16.png"});
-        if(bg.isTimerOn){
-            var message = Math.round(bg.limitSeconds / 60).toString() + "分かかると見積もった作業を" + Math.round(bg.elapsedSeconds / 60).toString() + "分で終えました!" + new Date().toString();
-            bg.tweet(message, function(){ bg.alert("tweetしたよ^_^");});
-        }
+        var message = Math.round(bg.limitSeconds / 60).toString() + "分かかると見積もった作業を" + Math.round(bg.elapsedSeconds / 60).toString() + "分で終えました!" + new Date().toString();
+        bg.tweet(message, function(){ bg.alert("tweetしたよ^_^"); });
         bg.stopTimer();
+        flushButtonArea();
     });
+    flushButtonArea();
 });
