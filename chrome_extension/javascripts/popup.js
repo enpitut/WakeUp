eacfab2f3ff497e38312f7f5e22b0e1f171950a4
watanabe <wtnabe.rei@gmail.com>
2015-10-04 19:40:16 +0900

    ポップアップ画面内のボタンをクリックするとNGサイトを消去できるような機能を追加

--- chrome_extension/javascripts/popup.js
+++ chrome_extension/javascripts/popup.js
@@ -26,12 +26,36 @@ $(() => {
     });
     flushButtonArea();
     
+    $("#register_ngsite_button").ready(() => {
+        chrome.tabs.query({currentWindow: true, active: true}, tabs => {
+            let currentTab = tabs[0];
+            let urlList = JSON.parse(localStorage.getItem("urlList"));
+            var domain = (currentTab.url.split("/"))[2];
+            
+            var index = urlList.findIndex(url => url == domain);
+            
+            if(index == -1) {
+                $("#register_ngsite_button").val("開いているタブをNGサイトに登録する");
+            } else {
+                $("#register_ngsite_button").val("開いているタブをNGサイトから除外する");
+            }
+        });
+    });
     $("#register_ngsite_button").click(() => {
         chrome.tabs.query({currentWindow: true, active: true}, tabs => {
-                let currentTab = tabs[0];
-                let urlList = JSON.parse(localStorage.getItem("urlList"));
-                urlList.push(currentTab.url);
-                localStorage.setItem("urlList", JSON.stringify(urlList));
+            let currentTab = tabs[0];
+            let urlList = JSON.parse(localStorage.getItem("urlList"));
+            var domain = (currentTab.url.split("/"))[2];
+            
+            var index = urlList.findIndex(url => url == domain);
+            
+            if(index == -1) {
+                urlList.push(domain);
+            } else {
+                urlList.splice(index, 1);
+            }
+            
+            localStorage.setItem("urlList", JSON.stringify(urlList));
         });
     });
 });
