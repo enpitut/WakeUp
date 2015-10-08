a2ed82492f9679e9ff62c4e350346c0e531e57db
dulltz <dulltz@outlook.com>
2015-10-08 22:52:10 +0900

    ポップアップ内の案内メッセージをタイマーのオンオフに合わせて切り替える機能を追加

--- chrome_extension/javascripts/popup.js
+++ chrome_extension/javascripts/popup.js
@@ -11,18 +11,28 @@ $(() => {
             $("#end_button").parent().css("display", "none");
         }
     }
+    function refreshGuideMessage() {
+        if (!bg.isTimerOn) {
+            $(".guide_message").html("ボタンを押すと監視がはじまるよ！");
+        } else {
+            $(".guide_message").html("監視中");
+        }
+    }
 
     $("#start_button").click(() => {
         let time = Number($("#task_time_text").val()) * 60;
         if(isNaN(time) || time < 0) return false;
         bg.startTimer(time);
         flushButtonArea();
+        refreshGuideMessage();
     });
     $("#end_button").click(() => {
         let message = `${Math.round(bg.limitSeconds / 60)}分かかると見積もった作業を${Math.round(bg.elapsedSeconds / 60)}分で終えました! #UGEN ${new Date()}`;
         bg.tweet(message, () => { bg.alert("tweetしたよ^_^"); });
         bg.stopTimer();
         flushButtonArea();
+        refreshGuideMessage();
     });
     flushButtonArea();
+    refreshGuideMessage();
 });
