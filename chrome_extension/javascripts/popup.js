5e1455ecd624fb15c96e04dfdc22b2e04d63c8bf
strangestorage <watanabe@watanaberei-no-MacBook-Air.local>
2015-10-11 03:46:45 +0900

    コンテキストメニュー追加

--- chrome_extension/javascripts/popup.js
+++ chrome_extension/javascripts/popup.js
@@ -25,38 +25,4 @@ $(() => {
         flushButtonArea();
     });
     flushButtonArea();
-    
-    $("#register_ngsite_button").ready(() => {
-        chrome.tabs.query({currentWindow: true, active: true}, tabs => {
-            let currentTab = tabs[0];
-            let urlList = JSON.parse(localStorage.getItem("urlList"));
-            var domain = (currentTab.url.split("/"))[2];
-            
-            var index = urlList.findIndex(url => url == domain);
-            
-            if(index == -1) {
-                $("#register_ngsite_button").val("開いているタブをNGサイトに登録する");
-            } else {
-                $("#register_ngsite_button").val("開いているタブをNGサイトから除外する");
-            }
-        });
-    });
-    $("#register_ngsite_button").click(() => {
-        chrome.tabs.query({currentWindow: true, active: true}, tabs => {
-            let currentTab = tabs[0];
-            let urlList = JSON.parse(localStorage.getItem("urlList"));
-            var domain = (currentTab.url.split("/"))[2];
-            
-            var index = urlList.findIndex(url => url == domain);
-            
-            if(index == -1) {
-                urlList.push(domain);
-            } else {
-                urlList.splice(index, 1);
-            }
-            
-            localStorage.setItem("urlList", JSON.stringify(urlList));
-        });
-    });
 });
-
