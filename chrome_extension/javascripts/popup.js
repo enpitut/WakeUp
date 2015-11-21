231927fc086f85cfc4467805779d957e8357fc6f
dulltz <isrgnoe@gmail.com>
2015-11-21 09:46:24 +0900

    コメント https://github.com/enpitut/WakeUp/pull/50#issuecomment-158523161 に基づいた修正

--- chrome_extension/javascripts/popup.js
+++ chrome_extension/javascripts/popup.js
@@ -58,7 +58,7 @@ $(() => {
     });
     $("#end_button").click(() => {
         bg.tweet(bg.generateTweet(
-            element => sprintf(tweet_phrases.phrases.successed[Math.floor(Math.random() * tweet_phrases.phrases.successed.length)], Math.round(bg.limitSeconds / 60), element, Math.round(bg.elapsedSeconds / 60), new Date()),
+            element => sprintf(TWEET_PHRASES.SUCCESSED, Math.round(bg.limitSeconds / 60), element, Math.round(bg.elapsedSeconds / 60), new Date()),
             {
                 element: bg.taskDescription,
                 formatter(element, upperLimitLength, getShortenedString) {
--- chrome_extension/javascripts/tweet_phrases.js
+++ chrome_extension/javascripts/tweet_phrases.js
@@ -1,22 +1,8 @@
-const tweet_phrases = {
-    "name": "UGEN tweet 文言集",
-    "version": "0.0.1",
-    "description": "UGENでツイートするときの文言集",
-    "phrases": {
-        "watched_ngsites": {
-            "include_tabinfo": [
-                "私は%sをさぼってるよ。今のタブはこれ！ %s( %s ) %s #UGEN",
-                "私は%sをサボって %s( %s ) を見ていました %s #UGEN"
-            ],
-            "not_include_tabinfo": [
-                "私は%sをサボっていました %s #UGEN"
-            ]
-        },
-        "failed": [
-            "@%s 突然のリプライ失礼致します。このたび私事ながら作業時間の見積もりに失敗しました。誠に申し訳ありません %s #UGEN"
-        ],
-        "successed": [
-            "%d分かかると見積もった%sを%d分で終えました! #UGEN %s"
-        ]
-    }
+const TWEET_PHRASES = {
+    "WATCHED_NGSITES": {
+        "WITH_TABINFO":     "私は%sをサボって %s( %s ) を見ていました %s #UGEN",
+        "WITHOUT_TABINFO":  "私は%sをサボっていました %s #UGEN"
+    },
+    "FAILED":       "@%s 突然のリプライ失礼致します。このたび私事ながら作業時間の見積もりに失敗しました。誠に申し訳ありません %s #UGEN",
+    "SUCCESSED":    "%d分かかると見積もった%sを%d分で終えました! #UGEN %s"
 };
